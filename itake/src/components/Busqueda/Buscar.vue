7615eacdb9d1f9446720d5454116942addedcf70
CarolinsRainbow <carolina__herrera@live.com>
2020-06-23 16:08:07 -0400

    Navbar

---
+++ itake/src/components/Busqueda/Buscar.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Aqui van perritos</h1>
+    </div>
+</template>
--- itake/src/views/Busqueda.vue
+++ itake/src/views/Busqueda.vue
@@ -1,10 +1,16 @@
 <template>
-
-    
+<div>
+        <Navbar></Navbar>
+    </div>
 </template>
 
 <script>
+import Navbar from "@/components/Navbar.vue";
+
 export default {
     name: 'Busqueda',
+  components: {
+    Navbar
+  },
 }
 </script>
