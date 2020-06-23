e1646214b9b4cc9cbb719dca4c016ee3b6bb4b52
CarolinsRainbow <carolina__herrera@live.com>
2020-06-23 09:25:10 -0400

    Importación de comandos
    Se ingreso en Routes + Main JS

--- itake/src/main.js
+++ itake/src/main.js
@@ -2,8 +2,22 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import Firebase from 'firebase'
+
+//Incorporamos Boostrap 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+
+Vue.use(BootstrapVue);
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+};
+
+
+Firebase.initializeApp(firebaseConfig)
 
 new Vue({
   router,
--- itake/src/router/index.js
+++ itake/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Firebase from 'firebase'
 
 Vue.use(VueRouter);
 
